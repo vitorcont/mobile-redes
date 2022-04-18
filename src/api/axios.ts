1ff2574094f4147162cc060b2b68572f25e16fbb
vitor-conti <vitor.conti@mblabs.com.br>
2022-04-18 09:27:20 -0300

    feat(base): base project

---
+++ src/api/axios.ts
@@ -0,0 +1,50 @@
+import { StorageItems } from '@mobile/enum/storage';
+import StorageService from '@mobile/services/storage';
+import Axios, { AxiosError, AxiosResponse } from 'axios';
+
+export enum AxiosStatus {
+  Unauthorized = 401,
+  Forbidden = 403,
+}
+
+interface IHandler {
+  unauthorizedError: () => void;
+}
+
+const handler: IHandler = {
+  unauthorizedError: () => { },
+};
+
+export const getInstance = async () => {
+  const accessToken = await StorageService.getItem(StorageItems.ACCESS_TOKEN);
+
+  const axiosInstance = Axios.create({
+    baseURL: 'yourapi.com',
+    timeout: 10000,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+  })
+
+  axiosInstance.interceptors.response.use(
+    (response: AxiosResponse) => response,
+    async (err: AxiosError) => {
+      if (err.response?.status === AxiosStatus.Unauthorized) {
+        handler.unauthorizedError();
+      } else if (err.response?.status === AxiosStatus.Forbidden) {
+        // your mechanism to forbidden
+      }
+
+      return Promise.reject();
+    },
+  );
+
+  return axiosInstance;
+};
+
+export const setHandleUnauthorizedError = (fn: () => void) => {
+  handler.unauthorizedError = fn;
+};
+
+export default getInstance;
